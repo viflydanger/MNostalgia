a983cfe4374eb0f10dd128c25ae162ed02ebdc1f
unknown <you@example.com>
2022-03-26 14:42:57 +0300

    com

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Main from './pg/Main.jsx'
+
+function App(){ 
+  return (
+    <div>
+      <Main />
+    </div>
+  );
+}
+export default App;
---
+++ src/pg/Main.jsx
@@ -0,0 +1,9 @@
+const Main = () => {
+	return (
+		<div className="App">
+		<div>text</div>
+		</div>
+	);
+}
+
+export default Main;
